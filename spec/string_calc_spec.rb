1f253f9593a32e7fde71251f505d797ba1860952
ALRW <awerner1@googlemail.com>
2015-10-28 17:17:29 +0000

    completed first stage

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -1,1 +1,19 @@
-require 'string'
+require 'string_calc'
+
+describe '#intAdd' do
+  context 'given and empty string' do
+    it 'should return 0' do
+      expect(intAdd("")).to eq(0)
+    end
+  end
+  context 'given one number' do
+    it 'should return the number as an integer' do
+      expect(intAdd("5")).to eq(5)
+    end
+  end
+  context 'given two numbers' do
+    it 'should return the sum of the two numbers as an integer' do
+      expect(intAdd("5,6")).to eq(11)
+    end
+  end
+end
